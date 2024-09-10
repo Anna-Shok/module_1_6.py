6c8a17f1328213eef678eb45b0ccbbc1de7e0bda
Anna-Shok <417496anna@mail.ru>
2024-09-10 23:24:33 +0300

    Add files via upload

---
+++ module_1_6.py
@@ -0,0 +1,26 @@
+my_dict = {'Ivan': 1995, 'Eva': 1967, 'Misha': 2003}
+print(my_dict)
+
+print(my_dict ['Eva'])
+
+my_dict['Olya'] = 1939
+print(my_dict)
+
+my_dict.update ({'Alla': 1984, 'Mark': 1910})
+print(my_dict)
+
+my_dict.pop('Misha')
+print(my_dict)
+
+my_set = {1, 2, 3, 54, 54, 'apple', 3, 9, 'apple'}
+print(my_set)
+
+print(my_set.add(37))
+print(my_set)
+
+meaning = (2, 3, 7)
+print(my_set.add(meaning))
+print(my_set)
+
+print(my_set.remove(54))
+print(my_set)
